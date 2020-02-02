01e077b0d841b0bac09da83fcd7ac03bae4d50c6
Artur Movsisyan <arturmovsisyan@workfront.com>
2020-02-02 16:37:08 +0400

    state full changes

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,23 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+
+class MyAppState extends State<MyApp> {
+  int questionIndex = 0;
+  List<String> questions = [
+    'Your favourite color ?',
+    'Your favourite animal ?',
+  ];
+
   @override
   Widget build(BuildContext context) {
-    const List<String> questions = [
-      'Your favourite color',
-      'Your favourite animal',
-    ];
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -17,22 +26,36 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text('The Question'),
+            Text(this.questions[this.questionIndex]),
             RaisedButton(
               child: Text('Answer 1'),
-              onPressed: null,
+              onPressed: () => this.answerQuestion(1),
             ),
             RaisedButton(
               child: Text('Answer 2'),
-              onPressed: null,
+              onPressed: () => this.answerQuestion(2),
             ),
             RaisedButton(
               child: Text('Answer 3'),
-              onPressed: null,
+              onPressed: () => this.answerQuestion(3),
             ),
           ],
         ),
       ),
     );
+  }
+
+  void answerQuestion(int answerId) {
+    int questionIndex = 0;
+    if (this.questionIndex <= this.questions.length - 2) {
+      questionIndex = this.questionIndex + 1;
+    }
+
+    setState(() {
+      this.questionIndex = questionIndex;
+    });
+
+    print(answerId);
+    print(this.questionIndex);
   }
 }
